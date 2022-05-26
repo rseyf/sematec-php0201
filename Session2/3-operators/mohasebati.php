7e8105317a8024dbe4d2a76add36d0a77605cb91
Reza <rseyf2017@gmail.com>
2022-05-26 16:00:42 +0430

    arthimetic operators added

---
+++ Session2/3-operators/mohasebati.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 10;
+    $b = 5;
+
+
+    var_dump($a + $b);
+    echo "<br>";
+    var_dump($a - $b);
+    echo "<br>";
+    var_dump($a * $b);
+    echo "<br>";
+    var_dump($a / $b);
+    echo "<br>";
+    
+    
+    var_dump($a % $b);
+    echo "<br>";
+    var_dump($a ** $b);
+    echo "<br>";
+    var_dump(-$a);
+    echo "<br>";
+    
