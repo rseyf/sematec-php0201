011a86824d305e6f1be4be6423abdef12939545b
Reza <rseyf2017@gmail.com>
2022-05-26 16:07:27 +0430

    added assignment operators

---
+++ Session2/3-operators/entesabi.php
@@ -0,0 +1,14 @@
+<?php
+    $a = 13;
+    
+    // using += 
+    $a += 14;   // --> hamoon $a = $a + 14;
+    var_dump($a); // output: 27
+    
+    // using -=
+    $a -= 3; // --> hamoon $a = $a - 3;
+    var_dump($a); // output: 27-3=24
+
+    // using *=
+    $a *= 4; // --> hamoon $a = $a * 4;
+    var_dump($a);
