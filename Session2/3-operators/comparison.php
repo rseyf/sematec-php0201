85490ddc1d525e659b045bda32656f8f9b05f346
Reza <rseyf2017@gmail.com>
2022-05-26 16:19:45 +0430

    comparison operators added to project

---
+++ Session2/3-operators/comparison.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<br>";
+    var_dump(3 < 3);
+    echo "<br>";
+    var_dump(2 <= 3);
+    echo "<br>";
+    var_dump(2 > 3);
+    echo "<br>";
+    var_dump(2 >= 3);
+    echo "<br>";
+    var_dump(8 >= 3);
+    echo "<br>";
+    var_dump(8 >= 8);
+    
