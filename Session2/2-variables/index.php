716ae4ae32143509a86cce9a379bddb106043471
Reza <rseyf2017@gmail.com>
2022-05-26 15:13:28 +0430

    variables section adde

---
+++ Session2/2-variables/index.php
@@ -0,0 +1,29 @@
+<?php
+    // integer (int)
+    // - yani maghadire adadi
+    $age = 30; 
+    var_dump($age);
+
+    echo ("</br>");
+
+
+    // boolean (bool)
+    //  - ya true
+    //  - ya false
+    $isActive = true;
+    var_dump($isActive);
+
+    echo ("</br>");
+
+    // float (float)
+    // - yani adaad ba ashaar
+    $myFloat = 3.1415;
+    var_dump($myFloat);
+
+    echo ("</br>");
+
+    //string (string)
+    // - reshteyi az matn 
+
+    $myMessage = "thanks to you";
+    var_dump($myMessage);
