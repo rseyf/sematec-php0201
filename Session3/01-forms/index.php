2de29fc6c154b7c6332358cdc2a08e0b65323772
Reza <rseyf2017@gmail.com>
2022-06-09 16:10:17 +0430

    post example

--- Session3/01-forms/index.php
+++ Session3/01-forms/index.php
@@ -1,6 +1,14 @@
 <?php
-    echo $_GET["customer_name"];
-    echo "<br>";
+    $customerName= $_POST["customer_name"];
+    echo ($_POST["customer_name"]);
+
+
+    // $sentEmail = mail("rseyf@hotmail.com", $customerName ,"new customer signed up");
+    // if ($sentEmail) {
+    //     echo "email sent!";
+    // }
+
+    
 
 ?>
 <html>
@@ -11,7 +19,7 @@
         <div><h1>Please Enter Your Name:</h1></div>
         <div>
             <!-- start of input form -->
-            <form action="index.php" enctype="multipart/form-data">
+            <form action="index.php" enctype="multipart/form-data" method="POST">
                 <input type="text" name="customer_name" placeholder="Your Name">
                 <button type="submit">Submit</button>
             </form>
