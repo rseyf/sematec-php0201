413bb4adea85ab47d6ba3824f1b4359b04621003
Reza Seyf <rseyf@hotmail.com>
2022-07-07 18:05:47 +0430

    pdo stuff

--- Session5/src/init.php
+++ Session5/src/init.php
@@ -7,9 +7,34 @@ $user = "root";
 $pass = "";
 $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-$query = "SELECT * FROM book";
 
-$rows = $db->query($query);
+// standard querying in php pdo
+
+// $query = "SELECT * FROM book WHERE author = :nevisande";
+// $statement = $db->prepare($query);
+// $statement->bindValue("nevisande","paulo");
+// $statement->execute();
+// $results = $statement->fetchAll();
+// foreach ($results as $radif) {
+//     echo $radif["title"] . "<br>";
+// }
+
+// $query = "UPDATE seyf1.book SET title = 'test22' where id = 8";
+// $db->query($query);
+
+
+// CRUD:
+// -------
+// Create
+// Read
+// Update
+// Delete
+
+$query2 = "DELETE FROM seyf1.book WHERE id = 8";
+$db->query($query2);
+
+// select * query
+$rows = $db->query("SELECT * FROM book");
 foreach ($rows as $radif) {
     echo "<pre>";
     echo $radif["title"];
@@ -17,5 +42,14 @@ foreach ($rows as $radif) {
     echo "</pre>";
 }
 
+
+// embedded line query (not recommended)
+// $query = <<<SQL
+// INSERT INTO seyf1.book (isbn,title,author,price) 
+// VALUES (91823719827,"farm animals","george orwel",6)
+// SQL;
+
+// $result = $db->exec($query);
+// var_dump($result);
 
 
